2a6a6728b05178548dd27490d6d5b2cfe6a83b1e
rallen0150 <rallen0150@gmail.com>
2016-11-16 20:45:38 -0500

    finally figured out some jquery problems with the side menu

--- foodtruck_finder/wsgi.py
+++ foodtruck_finder/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
+from whitenoise.django import DjangoWhiteNoise
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodtruck_finder.settings")
 
