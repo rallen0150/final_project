84b6d02eeb2a4632f257511b56dfe0c5f73db7f2
rallen0150 <rallen0150@gmail.com>
2016-11-17 10:26:04 -0500

    set up a s3 bucket and got some icons on the sidebar

--- foodtruck_finder/settings.py
+++ foodtruck_finder/settings.py
@@ -128,6 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
-# MEDIA_URL = '/static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_URL = '/static/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
