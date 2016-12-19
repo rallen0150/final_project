a3a05fea1dcaa0b5bc609d76d242fc5205116c7a
rallen0150 <rallen0150@gmail.com>
2016-12-19 18:42:38 -0500

    working better now

--- foodtruck_finder/wsgi.py
+++ foodtruck_finder/wsgi.py
@@ -6,8 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-from whitenoise.django import DjangoWhiteNoise
 
+from whitenoise.django import DjangoWhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
